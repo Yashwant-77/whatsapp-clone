a45cc8482e1e59a587f7c4ff6022c4fe1671dc9e
Yashwant-Chouhan <chouhanyashwant98@gmail.com>
2023-09-24 13:24:41 +0530

    half done

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,6 +4,8 @@ import { Avatar, IconButton } from "@mui/material";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import MicIcon from "@mui/icons-material/Mic";
 
 function Chat() {
   return (
@@ -35,6 +37,30 @@ function Chat() {
           This is a message
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
         </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Sonny</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form action="">
+          <input
+            type="text"
+            // value={input}
+            // onChange={(e) => SelectInput(e.target.value)}
+            placeholder="Type a message"
+          />
+          <button
+            // onClick={sendMessage}
+            type="submit"
+          >
+            Send a message
+          </button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
